cf315bcb3f74bc706f84143966d72082e2cc8c92
Gil Moreno <gilberto@edilgo.com>
2022-11-10 20:03:21 +0100

    [Removed contact and archive] In order to get going with the content

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,7 @@ export default function Header() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 pb-5">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <a href="/">
               <span className="sr-only text">Gilboss</span>
               <Logo fill="#3949AB"/>
             </a>
@@ -32,12 +32,12 @@ export default function Header() {
             <a href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
               About
             </a>
-            <a href="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            {/* <a href="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Contact
-            </a>
-            <a href="/archive" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            </a> */}
+            {/* <a href="/archive" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Archive
-            </a>
+            </a> */}
 
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
@@ -86,13 +86,13 @@ export default function Header() {
                   About
                 </a>
 
-                <a href="/contact" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                {/* <a href="/contact" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Contact
-                </a>
+                </a> */}
 
-                <a href="/archive" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                {/* <a href="/archive" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Archive
-                </a>
+                </a> */}
               </div>
               <div>
                 <a
--- src/routes/about.js
+++ src/routes/about.js
@@ -26,7 +26,7 @@ export default function About() {
           >Check out my CV</a>
           <a
             className="rounded-md border border-transparent bg-indigo-600 px-4 mx-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700" 
-            href="/contact"
+            href="mailto:contact@gil-moreno.com"
           >Get in touch</a>
         </div>
     </div>
